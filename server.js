59979d81f13b974fd086dd5a341bcaf6600b8bb4
frontend <frontenduchun@gmail.com>
2025-02-01 17:50:35 +0500

    The project is finished

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { createClient } = require('@supabase/supabase-js');
+const { createClient } = require("@supabase/supabase-js");
 const dotenv = require("dotenv");
 const TelegramBot = require('node-telegram-bot-api');
 
